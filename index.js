8da414ce465c036bf7bcf30b9cb73ae10970a36d
eyadse93 <eyadse93@gmail.com>
2016-04-08 21:37:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ var api = new ParseServer({
         production: false
       }
     }
-  });
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
